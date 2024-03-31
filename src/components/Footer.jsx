c1634384186c7c68cd485f2cb545e94555ce98b8
cbursch13 <cameron.bursch@target.com>
2024-03-31 00:05:59 -0500

    imported Footer to be used in App.jsx file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <a href="https://github.com/cbursch13" target="_blank" rel="noopener noreferrer">GitHub</a>
+        <a href="https://www.linkedin.com/in/cameron-bursch-a3028590/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        <a href="https://stackoverflow.com/users/youruserid/yourusername" target="_blank" rel="noopener noreferrer">Stack Overflow</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
